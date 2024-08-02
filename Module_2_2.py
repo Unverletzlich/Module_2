0e9ca1b010ad28b635dc5d62b70e63dbb4ae7b99
Unverletzlich <unverletzlich@gmail.com>
2024-08-02 17:55:41 +1100

    Add files via upload

--- Module_2_2.py
+++ Module_2_2.py
@@ -5,6 +5,6 @@ if first == second and third == first and second == third:
     print('3')
 elif first != second and first != third and second != third:
     print('0')
-elif first == second or first == third or second == third:
+elif first == second and first != third or first == third and first !=second or second == third and second!=first:
     print('2')
 
