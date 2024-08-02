ca9d5cd79955186f43f2092335e971d2856a1a17
Unverletzlich <unverletzlich@gmail.com>
2024-08-02 21:21:05 +1100

    Update Module_2_2_1.py

--- Module_2_2_1.py
+++ Module_2_2_1.py
@@ -1,5 +1,1 @@
-first = int(input('Enter the first number: '))
-second = int(input('Enter the second number: '))
-third = int(input('Enter the third number: '))
-a = list [first, [second], [third]]
-print (a[2])
+
